8fe03510899944f89d4e6305f9bff21f928660c5
gautamuniverse <gautamthapameriid@gmail.com>
2023-09-22 00:43:46 +0530

    Create Reverse a Doubly Linked List.java

---
+++ Linked List/Learn Doubly Linked List/Reverse a Doubly Linked List.java
@@ -0,0 +1,56 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node reverseDLL(Node head)
+    {
+        // Write your code here.
+
+        //base case
+        if(head.next == null)
+        {
+            return head;
+        }
+        
+        Node reversedHead = reverseDLL(head.next);
+        
+        Node temp = head;
+        temp = temp.next;
+        temp.next = head;
+        head.prev = temp;
+        head.next = null;
+
+        return reversedHead;
+    }
+}
