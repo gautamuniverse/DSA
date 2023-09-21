6e10336de185844aef79713858f2eea2f3c7c3d5
gautamuniverse <gautamthapameriid@gmail.com>
2023-09-22 00:13:54 +0530

    Update Search in a Linked List.java

---
+++ Linked List/Learn 1-D Linked List/Search in a Linked List.java
@@ -0,0 +1,47 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+
+public class Solution
+{
+    public static int searchInLinkedList(Node head, int k)
+    {
+        // Write Your Code Here.
+
+        while(head!=null)
+        {
+            if(head.data == k)
+            return 1;
+
+            head = head.next;
+        }
+
+        return 0;
+    }
+}s
