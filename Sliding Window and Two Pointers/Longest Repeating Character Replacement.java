f88d4e6e4200c5fab22c23cd1b651003bf0c55f1
gautamuniverse <gautamthapameriid@gmail.com>
2023-11-30 01:38:34 +0530

    Create Longest Repeating Character Replacement.java

---
+++ Sliding Window and Two Pointers/Longest Repeating Character Replacement.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public static int longestRepeatingSubstring(String str, int k) {
+        // Write your code here.
+
+        int left = 0;
+        int right;
+
+        int maxlength = 0;
+        int maxFreqChar = 0;
+
+        int[] char_arr = new int[26];
+
+        for (right = 0; right < str.length(); right++) {
+            maxFreqChar = Math.max(maxFreqChar, ++char_arr[str.charAt(right) - 'A']);
+            int window_size = right - left + 1;
+
+            // If the number of unique characters are more than k in the current window then
+            // we
+            // wont be able to make k replacements hence we need to reduce the window size.
+            int replaceCount = window_size - maxFreqChar;
+            if (replaceCount > k) {
+                // decrement the count of the left character in the char array
+                // Also increment the left pointer.w
+                --char_arr[str.charAt(left++) - 'A'];
+            } else // only calculate the maxLength when the replace count is less than k otherwise
+                   // we might end up calculating the maxLength fora substring with a maxReplace
+                   // count > k which is invalid
+                maxlength = Math.max(maxlength, window_size);
+        }
+
+        return maxlength;
+    }
+}
