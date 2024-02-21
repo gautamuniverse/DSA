042f397629625eb0f819716af1e9fc019d57c56f
gautamuniverse <gautamthapameriid@gmail.com>
2024-02-22 02:15:35 +0530

    Create LCA in Binary Tree.java

---
+++ Binary Trees/Hard/LCA in Binary Tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q)
+        return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null) return right;
+        else if(right == null) return left;
+
+        //If both are not equal to null that means the root is the lca
+        else return root;
+    }
+}
